77bb140e35d0424f86510ddd323ab6b729cac533
MaybeJaybe <69604238+MaybeJaybe@users.noreply.github.com>
2023-03-07 20:31:28 -0800

    finished tables

--- songs_albums.sql
+++ songs_albums.sql
@@ -1,25 +1,42 @@
 /*
- * TODO: Create a table called 'songs' that has the following fields:
- * - id - integer, primary key
- * - name - string
- * - album_id - foreign key
- * 
- * Note that album - song is a one-to-many relationship, so no bridge table is needed.
- */
+    * TODO: Create a table called 'songs' that has the following fields:
+    * - id - integer, primary key
+    * - name - string
+    * - album_id - foreign key
+    * 
+    * Note that album - song is a one-to-many relationship, so no bridge table is needed.
+*/
+
+CREATE TABLE Songs (
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    song_name VARCHAR(70) NOT NULL,
+    album_id INTEGER NOT NULL, 
+    FOREIGN KEY (album_id) REFERENCES Albums(id)
+);
 
 CREATE TABLE Albums (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name VARCHAR(70) NOT NULL,
+    album_name VARCHAR(70) NOT NULL,
     artist VARCHAR(70) NOT NULL,
     year_published INTEGER NOT NULL
 );
 
 /* 
- * TODO: Insert at least 4 rows of data into the songs table. You can change up the albums as well. :)
- */
- 
+    * TODO: Insert at least 4 rows of data into the songs table. You can change up the albums as well. :)
+*/
+
+INSERT INTO Songs
+    (song_name, album_id)
+VALUES
+    ('Eclipse', 1),
+    ('Something', 2),
+    ('Hotel California', 3),
+    ('Born in the U.S.A.', 4),
+    ('Kings of the Weekend', 5)
+;
+
 INSERT INTO Albums
-    (name, artist, year_published)
+    (album_name, artist, year_published)
 VALUES
     ('The Dark Side of the Moon', 'Pink Floyd', 1973),
     ('Abbey Road', 'The Beatles', 1969),
@@ -33,25 +50,39 @@ VALUES
 .mode column
 
 
+
 /* Queries */
 
--- SELECT * FROM Songs;
+SELECT * FROM Songs;
 
 SELECT * FROM Albums;
 
 /* 
- * TODO: Write a table join query to construct a table of Song Name : Album Name
- */
+    * TODO: Write a table join query to construct a table of Song Name : Album Name
+*/
+
+SELECT song_name, album_name FROM Albums
+JOIN Songs ON Albums.id = Songs.album_id;
 
 /*
- * TODO: Find all albums published between 1970 and 1980.
- */
+    * TODO: Find all albums published between 1970 and 1980.
+*/
+
+SELECT album_name FROM Albums
+WHERE year_published BETWEEN 1970 AND 1980;
 
 /*
- * TODO: Find all songs on albums published between 1970 and 1980. 
- *(Hint: Use a table join.)
- */
- 
+    * TODO: Find all songs on albums published between 1970 and 1980. 
+    * (Hint: Use a table join.)
+*/
+
+SELECT * FROM Albums
+JOIN Songs ON Albums.id = Songs.album_id
+WHERE year_published BETWEEN 1970 AND 1980;
+
 /*
- * TODO: Find all songs on albums with names containing 'California'.
- */
+    * TODO: Find all songs on albums with names containing 'California'.
+*/
+
+SELECT * FROM Albums
+WHERE album_name LIKE '%California%';
